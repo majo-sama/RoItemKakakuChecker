ffedc38086aab465128d0ff3a77dc6ef6bad7939
majo-sama <majo-sama@users.noreply.github.com>
2024-04-15 15:09:27 +0900

    自動アップデート機能を追加

--- forms/MainForm.cs
+++ forms/MainForm.cs
@@ -1,22 +1,18 @@
-﻿using System;
+﻿using Semver;
+using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http;
 using System.Reflection;
-using System.Security.Cryptography;
-using System.Speech.Synthesis;
 using System.Text;
-using System.Text.Json;
-using System.Text.Json.Serialization;
-using System.Text.RegularExpressions;
+using System.Text.Json.Nodes;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Windows.Forms.VisualStyles;
+using System.IO.Compression;
 
 
 namespace RoItemKakakuChecker
@@ -30,6 +26,8 @@ namespace RoItemKakakuChecker
         public OptionIdNameMap optionIdNameMap = new OptionIdNameMap();
         public Speaker speaker;
 
+        private const string VERSION = "1.3.0";
+
 
         public MainForm()
         {
@@ -41,7 +39,7 @@ namespace RoItemKakakuChecker
             chatObserveModeControl1.SetMainForm(this);
             speaker = new Speaker();
 
-            Application.ApplicationExit += Application_ApplicationExit;
+            System.Windows.Forms.Application.ApplicationExit += Application_ApplicationExit;
         }
 
         private void Application_ApplicationExit(object sender, EventArgs e)
@@ -49,7 +47,7 @@ namespace RoItemKakakuChecker
             speaker.Dispose();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private async void Form1_Load(object sender, EventArgs e)
         {
             storageObserveModeControl1.SetMainForm(this);
 
@@ -113,6 +111,130 @@ namespace RoItemKakakuChecker
 
             //起動
             p.Start();
+
+            this.Text = "RO価格確認機 v" + VERSION;
+
+
+            await AutoUpdate();
+        }
+
+        private async Task<string> AutoUpdate()
+        {
+            var currentSemVer = SemVersion.Parse(VERSION, SemVersionStyles.Strict);
+
+
+            try
+            {
+
+                string zipUrl = null;
+
+                using(var client = new HttpClient())
+                {
+
+                    var url = "https://api.github.com/repos/majo-sama/RoItemKakakuChecker/releases/latest";
+
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    var byteArray = Encoding.ASCII.GetBytes(":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");
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    client.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "RoItemKakakuChecker");
+
+                    HttpResponseMessage response = await client.GetAsync(url);
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        return "failed";
+                    }
+                    string json = await response.Content.ReadAsStringAsync();
+                    var parsedJson = JsonObject.Parse(json);
+                    var latestVersion = parsedJson["tag_name"].ToString();
+                    zipUrl = parsedJson["assets"][0]["browser_download_url"].ToString();
+
+                    var latestSemVer = SemVersion.Parse(latestVersion, SemVersionStyles.Strict);
+
+                    if (currentSemVer.CompareSortOrderTo(latestSemVer) >= 0)
+                    {
+                        return "no_needed";
+                    }
+
+                }
+
+
+                // 保存するパスを指定
+                string autoUpdateDir = "auto_update";
+                string zipFileName = "archive.zip";
+
+                if (Directory.Exists(autoUpdateDir))
+                {
+                    foreach (var f in Directory.EnumerateFiles(autoUpdateDir))
+                    {
+                        File.Delete(f);
+                    }
+                }
+                Directory.CreateDirectory("auto_update");
+                string filePath = $@"{autoUpdateDir}\{zipFileName}";
+
+                using (HttpClient zipClient = new HttpClient())
+                {
+                    zipClient.DefaultRequestHeaders.Accept.Clear();
+                    var bytes = Encoding.ASCII.GetBytes(":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");
+                    zipClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(bytes));
+                    zipClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/octet-stream"));
+                    zipClient.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "RoItemKakakuChecker");
+                    using (HttpResponseMessage zipResponse = await zipClient.GetAsync(zipUrl))
+                    {
+                        using (Stream streamToReadFrom = await zipResponse.Content.ReadAsStreamAsync())
+                        {
+                            using (Stream streamToWriteTo = File.Open(filePath, FileMode.Create))
+                            {
+                                await streamToReadFrom.CopyToAsync(streamToWriteTo);
+                            }
+                        }
+                    }
+                }
+
+
+                using (ZipArchive archive = ZipFile.OpenRead(filePath))
+                {
+                    var entries = archive.Entries.Where(e => e.FullName.EndsWith(".txt") || e.FullName.EndsWith(".exe") || e.FullName.EndsWith(".html"));
+                    foreach (ZipArchiveEntry entry in entries)
+                    {
+                        entry.ExtractToFile(Path.Combine(autoUpdateDir, entry.Name));
+                    }
+
+                }
+
+                // Batファイル出力
+                string text = "";
+                text += @"@echo Updating..." + "\r\n";
+                text += @"@echo off" + "\r\n";
+                text += @"timeout /t 5 /nobreak > nul" + "\r\n";
+                text += @"del " + System.Reflection.Assembly.GetExecutingAssembly().Location + " > nul" + "\r\n";
+                text += @"copy auto_update\RoItemKakakuChecker.exe RoItemKakakuChecker.exe" + "\r\n";
+                text += @"copy auto_update\readme.html readme.html" + "\r\n";
+                text += @"start RoItemKakakuChecker.exe" + "\r\n";
+                text += @"del patch.bat > nul" + "\r\n";
+                text += @"exit" + "\r\n";
+                StreamWriter sw = new StreamWriter(@"auto_update\patch.bat", false, Encoding.GetEncoding("Shift_JIS"));
+                sw.Write(text);
+                sw.Close();
+
+                // exe更新
+                System.Diagnostics.Process p = new System.Diagnostics.Process();
+                p.StartInfo.FileName = @"auto_update\patch.bat";
+                p.StartInfo.CreateNoWindow = true;
+                p.Start();
+                Application.Exit();
+                p.Close();
+                p = null;
+
+                //Directory.Delete(autoUpdateDir, true);
+
+                return "succeeded";
+            }
+            catch (Exception ex)
+            {
+                return "failed";
+            }
         }
 
         private void BtnStop_Click(object sender, EventArgs e)
